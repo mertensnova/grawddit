bc03fc8b86a5b7469052cda93fc74c80c9d2538d
amr8644 <amr8644@gmail.com>
2023-12-16 16:56:53 +0300

    Refactoring

--- main.go
+++ main.go
@@ -1,14 +1,40 @@
 package main
 
-var base_url string = "https://www.reddit.com"
 
+var base_url string = "https://reddit.com"
+
+func unique(intSlice []string) []string {
+	keys := make(map[string]bool)
+	list := []string{}
+	for _, entry := range intSlice {
+		if _, value := keys[entry]; !value {
+			keys[entry] = true
+			list = append(list, entry)
+		}
+	}
+	return list
+}
 func main() {
+
+	var data []Data
+
 	url := base_url + "/r/TwoSentenceHorror/top/?t=day"
+
 	html := SendRequests(url)
-	links := LinkGrabber(html)
+	links := unique(LinkGrabber(html))
 
 	for _, v := range links {
-       html := SendRequests(v)
-       GetTitle(html)
+		content := SendRequests(base_url + v)
+
+        id := GetText(content,"id")
+		var d = Data{
+            ID : id,
+			Title: GetText(content,"post-title"),
+            Score: GetText(content,"score"),
+            UserName: GetText(content,"author"),
+		}
+
+         GetPost(content,id)
+        data = append(data, d)
 	}
 }
--- scrape.go
+++ scrape.go
@@ -10,13 +10,77 @@ import (
 	"os"
 	"strings"
 	"time"
+
 	"golang.org/x/net/html"
 )
 
+type Data struct {
+    ID string
+	Title    string
+	Post     string
+	Score    string
+	UserName string
+}
 
+func GetPost(content string,id string)  {
+	reader := strings.NewReader(content)
+	doc, err := html.Parse(reader)
 
-func CheckUpvotes(html string) {
+	if err != nil {
+		log.Fatalln("Error parsing HTML:", err)
+	}
 
+	var traverse func(*html.Node)
+	traverse = func(node *html.Node) {
+		if node.Type == html.ElementNode && node.Data == "p" {
+            fmt.Println(node.FirstChild.Data)
+			/*for _, post := range node.Attr {
+				if post.Key == id {
+                    text = post.Val
+					break
+				}
+			}*/
+		}
+
+		for c := node.FirstChild; c != nil; c = c.NextSibling {
+			traverse(c)
+		}
+
+	}
+
+	traverse(doc)
+
+}
+
+func GetText(content string, attr string) string {
+
+    var text string
+	reader := strings.NewReader(content)
+	doc, err := html.Parse(reader)
+
+	if err != nil {
+		log.Fatalln("Error parsing HTML:", err)
+	}
+
+	var traverse func(*html.Node)
+	traverse = func(node *html.Node) {
+		if node.Type == html.ElementNode && node.Data == "shreddit-post" {
+			for _, post := range node.Attr {
+				if post.Key == attr {
+                    text = post.Val
+					break
+				}
+			}
+		}
+
+		for c := node.FirstChild; c != nil; c = c.NextSibling {
+			traverse(c)
+		}
+
+	}
+
+	traverse(doc)
+    return text
 }
 
 func LinkGrabber(value string) []string {
