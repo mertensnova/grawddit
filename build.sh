828c67d9013c337f60c3b27e8ea1502386e13f9c
Max Shegai <max.shegai@gmail.com>
2023-12-25 20:57:00 +0000

    move files to scrape package to preserve history

--- build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/zsh
-
-go build  -o grawddit main.go scrape.go utils.go target.go image.go
--- script.js
+++
@@ -1,3 +0,0 @@
-const scrollInterval = setInterval(() => {
-    window.scrollTo(0, document.body.scrollHeight)
-}, 500)
